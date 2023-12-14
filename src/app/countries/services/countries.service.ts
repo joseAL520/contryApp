32b624273d65ad63c3daa1d06f232e008fcc782a
JA <jose.albelt1817@gmail.com>
2023-12-14 16:15:18 -0500

    services catchError

--- src/app/countries/services/countries.service.ts
+++ src/app/countries/services/countries.service.ts
@@ -1,17 +1,21 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, catchError, of } from 'rxjs';
 import { Country } from '../interfaces/contry';
 
 @Injectable({providedIn: 'root'})
 export class ContriesService {
     
-  private url:string = 'https://restcountries.com/v3.1'
+  private urlService:string = 'https://restcountries.com/v3.1'
   constructor(private httpClient: HttpClient) { }
     
 
 
     searchCapital( contry:string):Observable<Country[]>  {
-        return this.httpClient.get<Country[]>(`${this.url}/capital/${contry}`);
+      const url = `${this.urlService}/capital/${contry}`
+        return this.httpClient.get<Country[]>(url)
+                .pipe( 
+                    catchError( () => of ([]) ) 
+                  );
     }
 }
